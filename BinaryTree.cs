fde7b6f0a42a588cec0803b5e5f7fdbf81452e71
Pahuka <J-a-c-k@yandex.ru>
2021-08-12 17:03:01 +0500

    Релизовал индексацию левого поддерева.

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -8,21 +8,42 @@ namespace BinaryTrees
     public class TreeNode<T> where T : IComparable
     {
         public T value { get; set; }
-        public TreeNode<T> Left, Right;
+        public TreeNode<T> Left, Right, Parent;
+        private int index = 0;
+        private int weight = 1;
 
-        public bool Contains(T value)
+        public void LeftIndex()
         {
-            var result = this.value.CompareTo(value);
-            if (Left != null) result = Left.value.CompareTo(value);
-            if (Right != null) result = Right.value.CompareTo(value);
-            return result == 0 ? true : false;
-        }        
+            var tempNode = Parent;
+            while (tempNode != null)
+            {
+                tempNode.weight += weight;
+                tempNode.index = weight;
+                tempNode = tempNode.Parent;
+            }
+        }
+
+        //public bool Contains(T value)
+        //{
+        //    var result = this.value.CompareTo(value);
+        //    if (Left != null) result = Left.value.CompareTo(value);
+        //    if (Right != null) result = Right.value.CompareTo(value);
+        //    return result == 0 ? true : false;
+        //}
     }
 
     public class BinaryTree<T> where T : IComparable
     {
         private List<TreeNode<T>> nodeList = new List<TreeNode<T>>();
         private TreeNode<T> root = new TreeNode<T>();
+
+        public TreeNode<T> this[int index]
+        {
+            get
+            {
+                return nodeList[index];
+            }
+        }
 
         public void Add(T value)
         {
@@ -33,26 +54,32 @@ namespace BinaryTrees
                 nodeList.Add(tempNode);
             }
 
-            else while (tempNode != null)
+            else
+            {
+                while (tempNode != null)
                 {
-                    if (tempNode.value.CompareTo(value) > 0)
                     {
-                        if (tempNode.Left == null)
+                        if (tempNode.value.CompareTo(value) > 0)
                         {
-                            tempNode.Left = new TreeNode<T> { value = value };
-                            nodeList.Add(tempNode.Left);
+                            if (tempNode.Left == null)
+                            {
+                                tempNode.Left = new TreeNode<T> { value = value, Parent = tempNode };
+                                tempNode.Left.LeftIndex();
+                                nodeList.Add(tempNode.Left);
+                                break;
+                            }
+                            tempNode = tempNode.Left;
+                        }
+                        else if (tempNode.Right == null)
+                        {
+                            tempNode.Right = new TreeNode<T> { value = value, Parent = tempNode };
+                            nodeList.Add(tempNode.Right);
                             break;
                         }
-                        tempNode = tempNode.Left;
+                        else tempNode = tempNode.Right;
                     }
-                    else if (tempNode.Right == null)
-                    {
-                        tempNode.Right = new TreeNode<T> { value = value };
-                        nodeList.Add(tempNode.Right);
-                        break;
-                    }
-                    else tempNode = tempNode.Right;
                 }
+            }
         }
 
         public bool Contains(T value)

7ae981bd4208709fafa065408b343103f812b420
Pahuka <J-a-c-k@yandex.ru>
2021-08-17 10:27:56 +0500

    Переделал метод Contains

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -10,32 +10,43 @@ namespace BinaryTrees
         public T value { get; set; }
         public TreeNode<T> Left, Right, Parent;
         private int index = 0;
-        private int weight = 1;
+        public int weight = 1;
 
-        public void LeftIndex()
+        public void UpDate()
         {
-            var tempNode = Parent;
-            while (tempNode != null)
-            {
-                tempNode.weight += weight;
-                tempNode.index = weight;
-                tempNode = tempNode.Parent;
-            }
+            if (this.value.CompareTo(value) <= 0) ;
+
+            //var tempNode = Parent;
+            //while (tempNode != null)
+            //{
+            //    if (tempNode.value.CompareTo(value) > 0)
+            //    {
+            //        Parent.index = weight;
+            //        Parent.weight++;
+            //    }
+            //    else
+            //    {
+            //        index = Parent.index + 1;
+            //        Parent.weight++;
+            //    }
+            //    tempNode = tempNode.Parent;
+            //}
         }
 
-        //public bool Contains(T value)
-        //{
-        //    var result = this.value.CompareTo(value);
-        //    if (Left != null) result = Left.value.CompareTo(value);
-        //    if (Right != null) result = Right.value.CompareTo(value);
-        //    return result == 0 ? true : false;
-        //}
+        public bool Contains(T value)
+        {
+            var result = this.value.CompareTo(value);
+            if (result == 0) return true;
+            else if (result > 0 && Left != null) return Left.Contains(value);
+            else if (result < 0 && Right != null) return Right.Contains(value);
+            else return false;
+        }
     }
 
     public class BinaryTree<T> where T : IComparable
     {
         private List<TreeNode<T>> nodeList = new List<TreeNode<T>>();
-        private TreeNode<T> root = new TreeNode<T>();
+        private TreeNode<T> root = new TreeNode<T>() { weight = 0 };
 
         public TreeNode<T> this[int index]
         {
@@ -48,48 +59,46 @@ namespace BinaryTrees
         public void Add(T value)
         {
             var tempNode = root;
-            if (nodeList.Count == 0)
+            if (tempNode.weight == 0)
             {
                 tempNode.value = value;
-                nodeList.Add(tempNode);
+                tempNode.weight++;
             }
 
             else
             {
                 while (tempNode != null)
                 {
+                    if (tempNode.value.CompareTo(value) > 0)
                     {
-                        if (tempNode.value.CompareTo(value) > 0)
+                        if (tempNode.Left == null)
                         {
-                            if (tempNode.Left == null)
-                            {
-                                tempNode.Left = new TreeNode<T> { value = value, Parent = tempNode };
-                                tempNode.Left.LeftIndex();
-                                nodeList.Add(tempNode.Left);
-                                break;
-                            }
-                            tempNode = tempNode.Left;
-                        }
-                        else if (tempNode.Right == null)
-                        {
-                            tempNode.Right = new TreeNode<T> { value = value, Parent = tempNode };
-                            nodeList.Add(tempNode.Right);
+                            tempNode.Left = new TreeNode<T> { value = value, Parent = tempNode };
+                            tempNode.Left.UpDate();
                             break;
                         }
-                        else tempNode = tempNode.Right;
+                        tempNode = tempNode.Left;
                     }
+                    else if (tempNode.Right == null)
+                    {
+                        tempNode.Right = new TreeNode<T> { value = value, Parent = tempNode };
+                        tempNode.Right.UpDate();
+                        break;
+                    }
+                    else tempNode = tempNode.Right;
                 }
             }
         }
 
         public bool Contains(T value)
         {
-            return nodeList.Select(x => x.value).Contains(value);
+            if (root.weight == 0) return false;
+            else return root.Contains(value);
         }
 
         public IEnumerator GetEnumerator()
         {
-            return nodeList.GetEnumerator();
+            return nodeList.Select(x => x.value).OrderBy(x => x).GetEnumerator();
         }
     }
 }
